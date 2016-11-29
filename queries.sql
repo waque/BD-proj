75ff230e0ea2215c288f4916ebc6e7fcf15322b4
João Ramos <joaoramos@Joaos-MacBook-Pro.local>
2016-11-29 21:07:01 +0000

    primeira querie corrigida

--- queries.sql
+++ queries.sql
@@ -1,18 +1,18 @@
 /*---------------first querie------------*/
 /*Quais os espacos com postos que nunca foram alugados?*/
-
-select * from espaco as E
+/*
+select distinct morada, codigo_espaco from posto as E
 where E.codigo not in ( select codigo
-						from aluga natural join oferta)
-
+						from aluga )
+*/
 /*--------------second querie------------*/
 /*Quais edificios com um numero de reservas superior a media?*/
-
+/*
 select morada, count(*) as cont from aluga
 group by morada
 having cont > (select avg(c) as average from (select morada, count(*) as c from aluga as a
 			group by morada) as b);
-
+*/
 /*----------------------------------------*/
 /*Quais utilizadores cujos alugaveis foram fiscalizados sempre pelo mesmo fiscal?*/
 
@@ -25,6 +25,10 @@ espaco foi alugado totalmente ou por postos*/
 
 
 /*---------------------------------------*/
-/*Qual o montante total realizado (pago) por cada espaço durante o ano de 2016?
-Assuma que a tarifa indicada na oferta é diaria. Deve considerar os casos em que o
-espaço foi alugado totalmente ou por postos*/
+/*Quais os espaços de trabalho cujos postos nele contidos foram todos alugados? (Por
+alugado entende-se um posto de trabalho que tenha pelo menos uma oferta aceite,
+independentemente das suas datas.)*/
+
+select distinct morada, codigo_espaco from posto as E
+where E.codigo not in ( select codigo
+						from aluga )
