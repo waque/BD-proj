e07b01f4433f38f1be7ec8d10487dd8fecbf6c74
João Ramos <joaoramos@Joaos-MacBook-Pro.local>
2016-12-01 12:58:16 +0000

    all queries done

--- queries.sql
+++ queries.sql
@@ -3,7 +3,7 @@
 /*
 select distinct morada, codigo_espaco from posto as E
 where E.codigo not in ( select codigo
-						from aluga )
+						from aluga);
 */
 /*--------------second querie------------*/
 /*Quais edificios com um numero de reservas superior a media?*/
@@ -15,20 +15,44 @@ having cont > (select avg(c) as average from (select morada, count(*) as c from 
 */
 /*----------------------------------------*/
 /*Quais utilizadores cujos alugaveis foram fiscalizados sempre pelo mesmo fiscal?*/
-
-
+/*
+select nif from (select * from arrenda natural join fiscaliza group by nif, id
+)as x group by nif having count(id) = 1;
+*/
 
 /*----------------------------------------*/
 /*Qual o montante total realizado (pago) por cada espaco durante o ano de 2016?
 Assuma que a tarifa indicada na oferta e diaria. Deve considerar os casos em que o
 espaco foi alugado totalmente ou por postos*/
 
+select morada, codigo as codigo_espaco, SUM(total) from
+	(select morada, codigo, total from ( /*Os que nao tem postos associados*/
+		select *, datediff(data_fim,data_inicio)*tarifa total
+		from aluga natural join oferta natural join paga) as big_data
+		group by morada, codigo, data_inicio
+		having (morada,codigo) not in (select morada,codigo from posto))p
+	group by morada, codigo;
+
+
+	select morada, codigo_espaco, SUM(total) from
+		(select morada, codigo, total from ( /*Os que tem postos associados*/
+			select *, datediff(data_fim,data_inicio)*tarifa total
+			from aluga natural join oferta natural join paga) as big_data
+			group by morada, codigo, data_inicio
+			having (morada,codigo) in (select morada, codigo from posto))p natural join posto
+		group by morada, codigo_espaco;
+
+
 
 /*---------------------------------------*/
 /*Quais os espaços de trabalho cujos postos nele contidos foram todos alugados? (Por
 alugado entende-se um posto de trabalho que tenha pelo menos uma oferta aceite,
 independentemente das suas datas.)*/
-
-select distinct morada, codigo_espaco from posto as E
-where E.codigo not in ( select codigo
-						from aluga )
+/*
+select distinct morada, codigo_espaco
+from posto p natural join aluga
+where p.codigo_espaco not in (
+	select distinct codigo_espaco from posto as E
+	where E.codigo not in ( select codigo
+		from aluga));
+*/
